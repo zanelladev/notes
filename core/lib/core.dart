2d5ed6f7ab45ccf2f97456eb19a0f761907a7ac6
zanelladev <arturf.zanella@gmail.com>
2023-07-26 01:32:37 -0300

    feat: working folder create with bd

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,1 +1,7 @@
 library core;
+
+export 'package:core/src/database/database_service.dart';
+export 'package:core/src/database/folders_db.dart';
+export 'package:core/src/database/notes_db.dart';
+export 'package:core/src/models/folder_model.dart';
+export 'package:core/src/models/note_model.dart';
